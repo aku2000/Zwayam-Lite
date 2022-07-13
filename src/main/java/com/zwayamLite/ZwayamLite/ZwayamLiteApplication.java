4e2eece79567f9a7f5f7f2e1e58a7c3c55baee99
aku2000 <akurox123@gmail.com>
2022-07-13 09:10:13 +0530

    adding springboot inital files

---
+++ src/main/java/com/zwayamLite/ZwayamLite/ZwayamLiteApplication.java
@@ -0,0 +1,13 @@
+package com.zwayamLite.ZwayamLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwayamLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwayamLiteApplication.class, args);
+	}
+
+}
